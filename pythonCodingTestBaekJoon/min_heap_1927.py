68d187ed08ea357a6f18280365e1ec47c6313ba8
Jeein Sophie Han <129861311+dev-jeein-0805@users.noreply.github.com>
2024-06-07 00:28:18 +0900

    Update min_heap_1927.py

--- pythonCodingTestBaekJoon/min_heap_1927.py
+++ pythonCodingTestBaekJoon/min_heap_1927.py
@@ -4,6 +4,8 @@ import heapq
 
 N = int(input())
 first_list = []
+
+# N 개의 요소를 리스트에 담고, 0 의 개수를 count() 함수 사용해서 zeroCount 에 저장
 for _ in range(N):
     first_list.append(int(input()))
 zeroCount = first_list.count(0)
@@ -12,6 +14,7 @@ min_heap = []
 
 output = []
 idx = 0  # first_list를 순회하기 위한 인덱스
+# 위에서 저장한 0 의 개수(=zeroCount) 만큼 for문 순회
 for _ in range(zeroCount):
     while idx < len(first_list):
         x = first_list[idx]
@@ -19,9 +22,11 @@ for _ in range(zeroCount):
         if x > 0:
             heapq.heappush(min_heap, x)
         elif x == 0:
+		        # min_heap 에 요소가 있을 때
             if min_heap:
                 smallest = heapq.heappop(min_heap)
                 output.append(smallest)
+            # min_heap 이 빈 [] 일 때
             else:
                 output.append(0)
 
